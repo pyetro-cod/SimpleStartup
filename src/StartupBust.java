48e678450f8f808e3cca5ec1755d47c40c8d006d
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2026-04-13 23:56:06 -0300

    Deleção da class SimpleStartuTestDrive

---
+++ src/StartupBust.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class StartupBust {
+    private GameHelper gameHelper = new GameHelper();
+
+    private ArrayList<Startup> startups = new ArrayList<>();
+
+    int numOfGuesses = 0;
+
+    void setUpGame() {
+    }
+
+    void startPlaying() {}
+        
+
+    void checkUserGuess() {
+    }
+
+    void finishGame() {
+    }
+
+}
