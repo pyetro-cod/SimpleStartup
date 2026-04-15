ad2a2c88f26a671dbbd4dd259cd30d9f020223d9
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2026-04-14 23:32:20 -0300

    Class StartupBust new método setUpGame

--- src/StartupBust.java
+++ src/StartupBust.java
@@ -7,10 +7,34 @@ public class StartupBust {
 
     int numOfGuesses = 0;
 
-    void setUpGame() {
+    private void setUpGame() {
+        Startup one = new Startup();
+        one.setName("poniez");
+        Startup  two = new Startup();
+        two.setName("hacqi");
+        Startup theree = new Startup();
+        theree.setName("cabista");
+        startups.add(one);
+        startups.add(two);
+        startups.add(theree);
+
+        System.out.println("Your goal is to sink three Startups.");
+        System.out.println("ponex,hacqi,cabista");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+
+        for(Startup startup : startups) {
+            ArrayList<String> newLocation = gameHelper.placeStartup(3);
+            startup.setLocationCells(newLocation);
+        }
     }
 
-    void startPlaying() {}
+    private void startPlaying() {
+        while (!startups.isEmpty()){
+            String useGuess = gameHelper.getUserInput("Enter a guess");
+            checkUserGuess(useGuess);
+        }
+        finishGame();
+    }
         
 
     void checkUserGuess() {
