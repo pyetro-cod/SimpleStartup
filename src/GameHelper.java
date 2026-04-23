684afb45a71d41b6a3521ad3154e5a3d8d4acb35
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2026-04-23 19:17:22 -0300

    Class GameHelperCompleta

--- src/GameHelper.java
+++ src/GameHelper.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -46,8 +45,64 @@ public class GameHelper {
             savepositionToGrid(startupCoords);
             ArrayList<String> alphaCells = convertCoordsToAlphaFormatet(startupCoords);
             System.out.println("Placed at: " + alphaCells);
-            return alphaCells
+            return alphaCells;
+
+            private boolean startupFits(int[] startupCoords, int increment){
+                int finalLocation = startupCoords[startupCoords.length - 1];
+                if (increment == HORINZONTAL_INCREMENT){
+                    return calcRowFromIndex(startupCoords[0]) == calcRomFromIndex(finalLocation);
+                } else {
+                    return finalLocation < GRID_SIZE;
+                }
+            }
+
+            private boolean coordsAvailable(int[] startupCoords){
+                for (int coord: startupCoords){
+                    if (grid[coord] != 0){
+//                        System.out.println("Position: " + coord + "already taken");
+                        return false;
+                    }
+
+                }
+                return true;
+            }
+
+            private void savePositionToGrid(int[] startupCoords){
+                for (int index : startupCoords){
+                    grid[index] = 1;
+                }
+            }
+
+            private ArrayList<String> convertCoordsToAlphaFormat(int[] startupCoords){
+                ArrayList<String> alphaCells = new ArrayList<String>();
+                for (int index : startupCoords){
+                    String alphaCoords = getAlphaCoordsFromIndex(index);
+                    alphaCells.add(alphaCoords);
+                }
+                return alphaCells;
+            }
+
+            private String getAlphaCoordsFromIndex(int index){
+                int row = calcRowFromIndex(index);
+                int column = index % GRID_LENGTH;
+                String letter = ALPHABET.substring(column,column+1);
+                return letter + row;
+            }
+
+            private int calcRowFromIndex(int index){
+                return index / GRID_LENGTH;
+            }
+
+            private int getIncrement(){
+                if (statupCount % 2 == 0){
+                    return HORINZONTAL_INCREMENT;
+                } else {
+                    return VERTICAL_INCREMENT;
+                }
+            }
         }
+
+
 
 
     }
